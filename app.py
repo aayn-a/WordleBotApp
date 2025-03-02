83b7ea3df9500b526530b1c91321becd5c0e1993
Aayan Agarwal <99608599+aayn-a@users.noreply.github.com>
2025-03-02 15:30:12 -0800

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,13 @@ def index():
 @app.route('/process_guess', methods=['POST'])
 def process_guess():
     guess = request.form['guess']
-    feedback = request.form['feedback'].split(',')
+    feedback = [request.form[f'feedback{i}'] for i in range(5)]
     global wordAnswers, startingWords
     wordAnswers, startingWords = wordEliminator(guess, feedback, wordAnswers, startingWords)
     newMap = calculations(wordAnswers, startingWords)
     sorted_map = dict(sorted(newMap.items(), key=lambda item: item[1], reverse=True))
     tupleList = list(sorted_map.items())
     best_guess = tupleList[0][0]
-    print(tupleList)
 
     return jsonify(best_guess=best_guess)
 
