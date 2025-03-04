32c5ecd85d20c6566eacd8e7095cfd9662f7f1ae
Aayan Agarwal <99608599+aayn-a@users.noreply.github.com>
2025-03-03 19:38:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__, template_folder='templates')
 
 wordAnswers = read_words_from_file("validWordleAnswers.txt")
 startingWords = read_words_from_file("wordlewords.txt")
+wordsGuessed = []
 
 @app.route('/')
 def index():
@@ -13,16 +14,29 @@ def index():
 
 @app.route('/process_guess', methods=['POST'])
 def process_guess():
-    guess = request.form['guess']
-    feedback = [request.form[f'feedback{i}'] for i in range(5)]
-    global wordAnswers, startingWords
-    wordAnswers, startingWords = wordEliminator(guess, feedback, wordAnswers, startingWords)
-    newMap = calculations(wordAnswers, startingWords)
-    sorted_map = dict(sorted(newMap.items(), key=lambda item: item[1], reverse=True))
-    tupleList = list(sorted_map.items())
-    best_guess = tupleList[0][0]
+    global wordAnswers, startingWords, wordsGuessed
 
-    return jsonify(best_guess=best_guess)
+    guess = request.form['guess']
+    if guess in wordsGuessed:
+        return jsonify(best_guess="Already guessed!")
+    else:
+        wordsGuessed.append(guess)
+        feedback = [request.form[f'feedback{i}'] for i in range(5)]
+        wordAnswers, startingWords = wordEliminator(guess, feedback, wordAnswers, startingWords)
+        newMap = calculations(wordAnswers, startingWords)
+        sorted_map = dict(sorted(newMap.items(), key=lambda item: item[1], reverse=True))
+        tupleList = list(sorted_map.items())
+        best_guess = tupleList[0][0]
+        return jsonify(best_guess=best_guess)
+@app.route("/reset", methods=["POST"])
+def reset():
+    global wordAnswers, startingWords, wordsGuessed
+    wordsGuessed = []
+    wordAnswers = read_words_from_file("validWordleAnswers.txt")
+    startingWords = read_words_from_file("wordlewords.txt")
+    return jsonify(message="Reset successful")
+
+
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
